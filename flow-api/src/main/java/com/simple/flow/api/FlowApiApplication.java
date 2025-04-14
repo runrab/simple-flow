8afeab079023bcdec61f92d417f25465ef346d81
mrcxw <1541649163@qq.com>
2025-04-14 13:34:42 +0800

    init

---
+++ flow-api/src/main/java/com/simple/flow/api/FlowApiApplication.java
@@ -0,0 +1,19 @@
+package com.simple.flow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述：
+ *
+ * @author runrab
+ * @date 2025/4/14 13:30
+ */
+@SpringBootApplication
+public class FlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowApiApplication.class, args);
+    }
+
+}
---
+++ flow-api/src/test/java/com/simple/flow/api/FlowApiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.simple.flow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 功能描述：
+ *
+ * @author runrab
+ * @date 2025/4/14 13:31
+ */
+@SpringBootTest
+class FlowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
